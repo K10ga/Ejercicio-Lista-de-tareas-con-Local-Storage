dd08903e2f9c51ca6065674aa22acf49ad08a054
armasaz <20243ds025@utez.edu.mx>
2025-10-15 13:44:26 -0600

    Inicio

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+const btnTareas = document.querySelector(".btnTareas");
+const pendientes = document.querySelector(".pendientes");
+
+const textoTarea = localStorage.getItem("texto");
+if(textoTarea != ""){
+    saludo.textContent=`Guardado: ${textGuardo}`;
+
+}
